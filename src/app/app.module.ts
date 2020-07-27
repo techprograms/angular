69b0b89a5be6611a64fbf604ccdee4f30055568b
technoprograms <tech.gpr@gmail.com>
2020-07-27 12:54:36 +0530

    Assignment Part 2 completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { ContactComponent } from './contact/contact.component';
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { LeaderService } from './services/leader.service';
 
 
 @NgModule({
@@ -39,7 +40,8 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
   ],
   providers: [
     DishService,
-    PromotionService
+    PromotionService,
+    LeaderService
   ],
   bootstrap: [AppComponent],
 })
